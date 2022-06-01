867a3f77f12c73cc344bc5a29e773cbf4bbe9f77
adding first <tanushikxdt2012@gmail.com>
2022-06-01 15:57:48 +0100

    adding first time

---
+++ js/main.js
@@ -0,0 +1,12 @@
+//самовызывающая функция
+(function () {
+const header = document.querySelector('.header');
+window.onscroll = () => {
+    if (window.pageYOffset > 50) {
+        header.classList.add('header_active')
+    } else { 
+        header.classList.remove('header_active')
+
+    }
+}
+}());
